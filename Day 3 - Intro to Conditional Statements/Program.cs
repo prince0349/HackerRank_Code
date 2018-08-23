913c8360620b9dbca94ed326263f988c2ed6018a
Alpesh Choubisa <alpesh.choubisa@wirestone.com>
2018-08-23 22:50:11 +0530

    Day 3: Intro to Conditional Statements of 30 days hacker rank

---
+++ Day 3 - Intro to Conditional Statements/Program.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+class Program
+{
+    static void Main(String[] args)
+    {
+        var N = int.Parse(Console.ReadLine());
+
+        if (N % 2 != 0) 
+			Console.WriteLine("Weird");
+        else
+        {
+            if (N <= 5) 
+				Console.WriteLine("Not Weird");
+            else if (N <= 20) 
+				Console.WriteLine("Weird");
+            else 
+				Console.WriteLine("Not Weird");
+        }
+    }
+}
